f2e463f5c614d4f90b9fd7e1d9d2290bdc45fce7
Touhma <Touhma@outlook.com>
2023-01-16 16:33:15 -0500

    https://catlikecoding.com/unity/tutorials/basics/mathematical-surfaces/

--- Assets/Scripts/Graph/Graph.cs
+++ Assets/Scripts/Graph/Graph.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using _Utils;
+using Unity.Mathematics;
+using UnityEngine;
 
 namespace Graph
 {
@@ -6,33 +8,36 @@ namespace Graph
     {
         [SerializeField] Transform pointPrefab;
         [SerializeField, Range(10, 100)] int resolution = 10;
+        [SerializeField] FunctionLibrary.FunctionName function;
 
         Transform[] points;
-        
-        void Awake()
+
+        public void Awake()
         {
             float step = 2f / resolution;
-            Vector3 position = Vector3.zero;
-            Vector3 scale = Vector3.one * step;
-            
-            points = new Transform[resolution];
-            
+            float3 scale = Vector3.one * step;
+            points = new Transform[resolution * resolution];
             for (int i = 0; i < points.Length; i++) {
-                Transform point = points[i] = Instantiate(pointPrefab, transform, false);
-                position.x = (i + 0.5f) * step - 1f;
-                point.localPosition = position;
+                Transform point = points[i] = Instantiate(pointPrefab);
                 point.localScale = scale;
+                point.SetParent(transform, false);
             }
         }
-        
-        void Update () {
+
+        public void Update()
+        {
+            FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
             float time = Time.time;
-            for (int i = 0; i < points.Length; i++)
-            {
-                Transform point = points[i];
-                Vector3 position = point.localPosition;
-                position.y = Mathf.Sin(Mathf.PI * (position.x + time));
-                point.localPosition = position;
+            float step = 2f / resolution;
+            float v = 0.5f * step - 1f;
+            for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++) {
+                if (x == resolution) {
+                    x = 0;
+                    z += 1;
+                    v = (z + 0.5f) * step - 1f;
+                }
+                float u = (x + 0.5f) * step - 1f;
+                points[i].localPosition = f(u, v, time);
             }
         }
     }
