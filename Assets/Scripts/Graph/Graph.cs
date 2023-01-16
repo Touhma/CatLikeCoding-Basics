d52aa130a39a2541f15acc23f5466bbac9f8e693
Touhma <Touhma@outlook.com>
2023-01-16 17:45:08 -0500

    https://catlikecoding.com/unity/tutorials/basics/measuring-performance/

--- Assets/Scripts/Graph/Graph.cs
+++ Assets/Scripts/Graph/Graph.cs
@@ -9,15 +9,31 @@ namespace Graph
         [SerializeField] Transform pointPrefab;
         [SerializeField, Range(10, 100)] int resolution = 10;
         [SerializeField] FunctionLibrary.FunctionName function;
+        [SerializeField, Min(0f)] float functionDuration = 1f, transitionDuration = 1f;
 
+        public enum TransitionMode
+        {
+            Cycle,
+            Random,
+            None
+        }
+
+        [SerializeField] TransitionMode transitionMode;
+        
+        FunctionLibrary.FunctionName transitionFunction;
+
+        bool transitioning;
+        
         Transform[] points;
+        float duration;
 
         public void Awake()
         {
             float step = 2f / resolution;
             float3 scale = Vector3.one * step;
             points = new Transform[resolution * resolution];
-            for (int i = 0; i < points.Length; i++) {
+            for (int i = 0; i < points.Length; i++)
+            {
                 Transform point = points[i] = Instantiate(pointPrefab);
                 point.localScale = scale;
                 point.SetParent(transform, false);
@@ -26,6 +42,37 @@ namespace Graph
 
         public void Update()
         {
+            if (transitionMode == TransitionMode.None)
+            {
+                UpdateFunction();
+            }
+            else
+            {
+                duration += Time.deltaTime;
+                if (transitioning) {
+                    if (duration >= transitionDuration) {
+                        duration -= transitionDuration;
+                        transitioning = false;
+                    }
+                }
+                else if (duration >= functionDuration) {
+                    duration -= functionDuration;
+                    transitioning = true;
+                    transitionFunction = function;
+                    PickNextFunction();
+                }
+
+                if (transitioning) {
+                    UpdateFunctionTransition();
+                }
+                else {
+                    UpdateFunction();
+                }
+            }
+           
+        }
+
+        void UpdateFunction () {
             FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
             float time = Time.time;
             float step = 2f / resolution;
@@ -39,6 +86,33 @@ namespace Graph
                 float u = (x + 0.5f) * step - 1f;
                 points[i].localPosition = f(u, v, time);
             }
+        }
+        
+        void UpdateFunctionTransition () {
+            FunctionLibrary.Function
+                from = FunctionLibrary.GetFunction(transitionFunction),
+                to = FunctionLibrary.GetFunction(function);
+            float progress = duration / transitionDuration;
+            float time = Time.time;
+            float step = 2f / resolution;
+            float v = 0.5f * step - 1f;
+            for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++) {
+                if (x == resolution) {
+                    x = 0;
+                    z += 1;
+                    v = (z + 0.5f) * step - 1f;
+                }
+                float u = (x + 0.5f) * step - 1f;
+                points[i].localPosition = FunctionLibrary.Morph(
+                    u, v, time, from, to, progress
+                );
+            }
+        }
+
+        void PickNextFunction () {
+            function = transitionMode == TransitionMode.Cycle ?
+                FunctionLibrary.GetNextFunctionName(function) :
+                FunctionLibrary.GetRandomFunctionNameOtherThan(function);
         }
     }
 }
--- Assets/Scripts/_Utils/FunctionLibrary.cs
+++ Assets/Scripts/_Utils/FunctionLibrary.cs
@@ -1,5 +1,6 @@
 ﻿using Unity.Mathematics;
 using static UnityEngine.Mathf;
+using Random = UnityEngine.Random;
 
 namespace _Utils
 {
@@ -16,61 +17,83 @@ namespace _Utils
             Torus
         }
 
-        static Function[] functions = { Wave, MultiWave, Ripple, Sphere,Torus };
+        private static Function[] functions = { Wave, MultiWave, Ripple, Sphere, Torus };
 
         public static Function GetFunction(FunctionName name)
         {
             return functions[(int)name];
         }
-
-        public static float3 Wave(float u, float v, float t)
-        {
-            return new float3(u, Sin(PI * (u + v + t)), v);
+        
+        public static FunctionName GetNextFunctionName (FunctionName name) {
+            return (int)name < functions.Length - 1 ? name + 1 : 0;
+        }
+        
+        public static FunctionName GetRandomFunctionName () {
+            var choice = (FunctionName)Random.Range(0, functions.Length);
+            return choice;
+        }
+        
+        public static FunctionName GetRandomFunctionNameOtherThan (FunctionName name) {
+            var choice = (FunctionName)Random.Range(1, functions.Length);
+            return choice == name ? 0 : choice;
         }
 
-        public static float3 MultiWave(float u, float v, float t)
+        private static float3 Wave(float u, float v, float t)
+        {
+            return new float3(u, math.sin(PI * (u + v + t)), v);
+        }
+
+        private static float3 MultiWave(float u, float v, float t)
         {
             float3 p;
             p.x = u;
-            p.y = Sin(PI * (u + 0.5f * t));
-            p.y += 0.5f * Sin(2f * PI * (v + t));
-            p.y += Sin(PI * (u + v + 0.25f * t));
+            p.y = math.sin(PI * (u + 0.5f * t));
+            p.y += 0.5f * math.sin(2f * PI * (v + t));
+            p.y += math.sin(PI * (u + v + 0.25f * t));
             p.y *= 1f / 2.5f;
             p.z = v;
             return p;
         }
 
-        public static float3 Ripple(float u, float v, float t)
+        private static float3 Ripple(float u, float v, float t)
         {
             float d = Sqrt(u * u + v * v);
             float3 p;
             p.x = u;
-            p.y = Sin(PI * (4f * d - t));
+            p.y = math.sin(PI * (4f * d - t));
             p.y /= 1f + 10f * d;
             p.z = v;
             return p;
         }
 
-        public static float3 Sphere(float u, float v, float t)
+        private static float3 Sphere(float u, float v, float t)
         {
-            float r = 0.9f + 0.1f * Sin(PI * (6f * u + 4f * v + t));
-            float s = r * Cos(0.5f * PI * v);
+            float r = 0.9f + 0.1f * math.sin(PI * (6f * u + 4f * v + t));
+            float s = r * math.cos(0.5f * PI * v);
             float3 p;
-            p.x = s * Sin(PI * u);
-            p.y = r * Sin(0.5f * PI * v);
-            p.z = s * Cos(PI * u);
+            p.x = s * math.sin(PI * u);
+            p.y = r * math.sin(0.5f * PI * v);
+            p.z = s * math.cos(PI * u);
             return p;
         }
-        
-        public static float3 Torus (float u, float v, float t) {
-            float r1 = 0.7f + 0.1f * Sin(PI * (6f * u + 0.5f * t));
-            float r2 = 0.15f + 0.05f * Sin(PI * (8f * u + 4f * v + 2f * t));
-            float s = r1 + r2 * Cos(PI * v);
+
+        private static float3 Torus(float u, float v, float t)
+        {
+            float r1 = 0.7f + 0.1f * math.sin(PI * (6f * u + 0.5f * t));
+            float r2 = 0.15f + 0.05f * math.sin(PI * (8f * u + 4f * v + 2f * t));
+            float s = r1 + r2 * math.cos(PI * v);
             float3 p;
-            p.x = s * Sin(PI * u);
-            p.y = r2 * Sin(PI * v);
-            p.z = s * Cos(PI * u);
+            p.x = s * math.sin(PI * u);
+            p.y = r2 * math.sin(PI * v);
+            p.z = s * math.cos(PI * u);
             return p;
+        }
+
+        public static float3 Morph(
+            float u, float v, float t, Function from, Function to, float progress
+        )
+        {
+            return math.unlerp(from(u, v, t), to(u, v, t), math.smoothstep(0f, 1f, progress));
         }
     }
 }
